89e924d807fd86c4d9247448cf35d94f2a2c2d45
WangZiXu <wzx1643735050@gmail.com>
2023-11-24 18:14:54 +0800

    debug局部变量错误，命名冲突；数据渲染步骤

--- static/js/echarts.js
+++ static/js/echarts.js
@@ -1,57 +1,100 @@
-function echarts_31() {
-  // 基于准备好的dom，初始化echarts实例
-  var echarts_31 = echarts.init(document.getElementById("fb1"));
-  option = {
-    tooltip: {
-      trigger: "item",
-      formatter: "{b}: {c} ({d}%)",
-      position: function (p) {
-        //其中p为当前鼠标的位置
-        return [p[0] + 10, p[1] - 10];
-      },
-    },
-    legend: {
-      top: "0%",
-      textStyle: {
-        color: "rgba(255,255,255,.5)",
-        fontSize: "12",
-      },
-    },
-    series: [
-      {
-        name: "当前图书馆在馆人数情况",
-        type: "pie",
-        color: [
-          "#065aab",
-          "#066eab",
-          "#0682ab",
-          "#0696ab",
-          "#06a0ab",
-          "#06b4ab",
-          "#06c8ab",
-          "#06dcab",
-          "#06f0ab",
-        ],
-        label: {
-          show: true, // 显示标签
-          position: "inside", // 标签显示在饼图内部
-          formatter: "{b}:{c}({d}%)", // 设置标签的显示格式
-          textStyle: {
-            color: "rgba(255,255,255,.5)", // 标签文字颜色
-            fontSize: "14",
-          },
-        },
-        labelLine: { show: false },
-      },
-    ],
+function startClock() {
+  var t = null;
+  t = setTimeout(updateTime, 1000); //开始运行
+
+  function updateTime() {
+    clearTimeout(t); //清除定时器
+    var dt = new Date();
+    var y = dt.getFullYear();
+    var mt = dt.getMonth() + 1;
+    var day = dt.getDate();
+    var h = dt.getHours(); //获取时
+    var m = dt.getMinutes(); //获取分
+    var s = dt.getSeconds(); //获取秒
+    document.getElementById("showTime").innerHTML =
+      y +
+      "年" +
+      mt +
+      "月" +
+      day +
+      "日" +
+      "-" +
+      h +
+      "时" +
+      m +
+      "分" +
+      s +
+      "秒";
+    t = setTimeout(updateTime, 1000); //设定定时器，循环运行
+  }
+
+  // 在页面加载完成后自动开始时钟
+  window.onload = function () {
+    startClock();
   };
-  // 使用刚指定的配置项和数据显示图表。
-  echarts_31.setOption(option);
+}
+var echarts_31
+var echarts_32
+var echarts_4
+function echarts31() {
+  // 基于准备好的dom，初始化echarts实例
+  echarts_31 = echarts.init(document.getElementById("fb1"));
+  $.get("/api/library_stats").done(function (data) {
+    // 根据获取的数据更新图表的配置项和数据
+    var option = {
+      tooltip: {
+        trigger: "item",
+        formatter: "{b}: {c} ({d}%)",
+        position: function (p) {
+          //其中p为当前鼠标的位置
+          return [p[0] + 10, p[1] - 10];
+        },
+      },
+      legend: {
+        top: "0%",
+        data: data.xAxis,
+        textStyle: {
+          color: "rgba(255,255,255,.5)",
+          fontSize: "12",
+        },
+      },
+      series: [
+        {
+          name: "当前图书馆在馆人数情况",
+          type: "pie",
+          data: data.data,
+          color: [
+            "#065aab",
+            "#066eab",
+            "#0682ab",
+            "#0696ab",
+            "#06a0ab",
+            "#06b4ab",
+            "#06c8ab",
+            "#06dcab",
+            "#06f0ab",
+          ],
+          label: {
+            show: true, // 显示标签
+            position: "inside", // 标签显示在饼图内部
+            formatter: "{b}:{c}({d}%)", // 设置标签的显示格式
+            textStyle: {
+              color: "rgba(255,255,255,.5)", // 标签文字颜色
+              fontSize: "14",
+            },
+          },
+          labelLine: { show: false },
+        },
+      ],
+    };
+    // 将更新后的配置项设置给图表
+    echarts_31.setOption(option);
+  });
   window.addEventListener("resize", function () {
     echarts_31.resize();
   });
 }
-function updateChart_31() {
+function updateChart31() {
   // 发起异步请求获取数据
   $.get("/api/library_stats").done(function (data) {
     // 根据获取的数据更新图表的配置项和数据
@@ -72,104 +115,111 @@ function updateChart_31() {
   });
 }
 
-function echarts_32() {
-  var echarts_32 = echarts.init(document.getElementById("fb2"));
-  option = {
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        type: "shadow",
-      },
-    },
-    grid: {
-      top: "10%",
-      left: "3%",
-      right: "3%",
-      bottom: "3%", //调整边距，使柱状图能够更好地占据块内的空间
-      containLabel: true,
-    },
-    yAxis: {
-      type: "value",
-      axisLine: {
-        lineStyle: {
-          color: "rgba(255,255,255,.5)",
+function echarts32() {
+  echarts_32 = echarts.init(document.getElementById("fb2"));
+  $.get("/api/dept_inlib").done(function (data) {
+    // 根据获取的数据更新图表的配置项和数据
+    var option = {
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
         },
       },
-      axisLabel: {
-        textStyle: {
-          color: "rgba(255,255,255,.5)",
-          fontSize: "12",
-        },
+      grid: {
+        top: "10%",
+        left: "3%",
+        right: "3%",
+        bottom: "3%", //调整边距，使柱状图能够更好地占据块内的空间
+        containLabel: true,
       },
-      splitLine: {
-        lineStyle: {
-          color: "rgba(255,255,255,.1)",
-        },
-      },
-    },
-    legend: {
-      // top:'0%',
-      textStyle: {
-        color: "rgba(255,255,255,.5)",
-        fontSize: "12",
-      },
-    },
-    xAxis: {
-      type: "category",
-      axisLine: {
-        lineStyle: {
-          color: "rgba(255,255,255,.5)",
-        },
-      },
-      axisLabel: {
-        textStyle: {
-          show: true,
-          color: "rgba(255,255,255,.5)",
-          fontSize: "12",
-        },
-      },
-    },
-    series: [
-      {
-        name: "{{form.echarts3_2.title}}",
-        type: "bar",
-        barWidth: "100%", //柱子宽度
-        // barGap: 1, //柱子之间间距
-        itemStyle: {
-          color: function (params) {
-            // 定义基准颜色
-            var baseColor1 = "#087a73";
-            var baseColor2 = "#6a1479";
-            // 生成颜色插值器
-            var colorInterpolator = chroma
-              .scale([baseColor1, baseColor2])
-              .mode("lch")
-              .colors(10);
-            // 定义颜色数组
-            var colorList = colorInterpolator.map(function (color) {
-              return color;
-            });
-            return colorList[params.dataIndex % colorList.length];
+      yAxis: {
+        type: "value",
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.5)",
           },
         },
-        label: {
-          show: true, // 显示标签
-          position: "outside", // 标签显示在直方图外部
-          formatter: "{b}", // 设置标签的显示格式
+        axisLabel: {
           textStyle: {
-            color: "rgba(255,255,255,.5)", // 标签文字颜色
+            color: "rgba(255,255,255,.5)",
+            fontSize: "12",
+          },
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+      },
+      legend: {
+        // top:'0%',
+        data: data.xAxis,
+        textStyle: {
+          color: "rgba(255,255,255,.5)",
+          fontSize: "12",
+        },
+      },
+      xAxis: {
+        type: "category",
+        data: data.data,
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.5)",
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            show: true,
+            color: "rgba(255,255,255,.5)",
             fontSize: "12",
           },
         },
       },
-    ],
-  };
-  echarts_32.setOption(option);
+      series: [
+        {
+          name: "",
+          type: "bar",
+          barWidth: "100%", //柱子宽度
+          // barGap: 1, //柱子之间间距
+          data: data.data,
+          itemStyle: {
+            color: function (params) {
+              // 定义基准颜色
+              var baseColor1 = "#087a73";
+              var baseColor2 = "#6a1479";
+              // 生成颜色插值器
+              var colorInterpolator = chroma
+                .scale([baseColor1, baseColor2])
+                .mode("lch")
+                .colors(10);
+              // 定义颜色数组
+              var colorList = colorInterpolator.map(function (color) {
+                return color;
+              });
+              return colorList[params.dataIndex % colorList.length];
+            },
+          },
+          label: {
+            show: true, // 显示标签
+            position: "outside", // 标签显示在直方图外部
+            formatter: "{b}", // 设置标签的显示格式
+            textStyle: {
+              color: "rgba(255,255,255,.5)", // 标签文字颜色
+              fontSize: "12",
+            },
+          },
+        },
+      ],
+    };
+    // 将更新后的配置项设置给图表
+    echarts_32.setOption(option);
+  });
   window.addEventListener("resize", function () {
     echarts_32.resize();
   });
 }
-function updateChart_32() {
+function updateChart32() {
   // 发起异步请求获取数据
   $.get("/api/dept_inlib").done(function (data) {
     // 根据获取的数据更新图表的配置项和数据
@@ -193,151 +243,157 @@ function updateChart_32() {
   });
 }
 
-function echarts_4() {
+function echarts4() {
   // 基于准备好的dom，初始化echarts实例
-  var echarts_4 = echarts.init(document.getElementById("echart4"));
-  option = {
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        lineStyle: {
-          color: "#dddc6b",
-        },
-      },
-      formatter: function (params) {
-        var result = params[0].name + "<br/>"; // x轴数据
-        for (var i = 0; i < params.length; i++) {
-          var seriesName = params[i].seriesName; // 系列名称
-          var value = params[i].value; // 数据值
-          result += seriesName + ": " + value + "%<br/>"; // 拼接每个系列的名称和数据值
-        }
-        return result;
-      },
-    },
-    grid: {
-      left: "10",
-      top: "30",
-      right: "10",
-      bottom: "10",
-      containLabel: true,
-    },
-    xAxis: [
-      {
-        type: "category",
-        boundaryGap: false,
-        axisLabel: {
-          textStyle: {
-            color: "rgba(255,255,255,.6)",
-            fontSize: 12,
-          },
-        },
-        axisLine: {
+  echarts_4 = echarts.init(document.getElementById("echart4"));
+  $.get("/api/hourly_utilization").done(function (data) {
+    // 根据获取的数据更新图表的配置项和数据
+    var option = {
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
           lineStyle: {
-            color: "rgba(255,255,255,.2)",
+            color: "#dddc6b",
           },
         },
+        formatter: function (params) {
+          var result = params[0].name + "<br/>"; // x轴数据
+          for (var i = 0; i < params.length; i++) {
+            var seriesName = params[i].seriesName; // 系列名称
+            var value = params[i].value; // 数据值
+            result += seriesName + ": " + value + "%<br/>"; // 拼接每个系列的名称和数据值
+          }
+          return result;
+        },
       },
-      {
-        axisPointer: { show: false },
-        axisLine: { show: false },
-        position: "bottom",
-        offset: 20,
+      grid: {
+        left: "10",
+        top: "30",
+        right: "10",
+        bottom: "10",
+        containLabel: true,
       },
-    ],
-    yAxis: [
-      {
-        type: "value",
-        min: 0, // 设置最小值为0
-        max: 100, // 设置最大值为100
-        axisTick: { show: false },
-        axisLine: {
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: false,
+          axisLabel: {
+            textStyle: {
+              color: "rgba(255,255,255,.6)",
+              fontSize: 12,
+            },
+          },
+          axisLine: {
+            lineStyle: {
+              color: "rgba(255,255,255,.2)",
+            },
+          },
+          data: data.data,
+        },
+        {
+          axisPointer: { show: false },
+          axisLine: { show: false },
+          position: "bottom",
+          offset: 20,
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+          min: 0, // 设置最小值为0
+          max: 100, // 设置最大值为100
+          axisTick: { show: false },
+          axisLine: {
+            lineStyle: {
+              color: "rgba(255,255,255,.1)",
+            },
+          },
+          axisLabel: {
+            formatter: "{value}%", // 设置刻度标签的格式为百分比形式
+            textStyle: {
+              color: "rgba(255,255,255,.6)",
+              fontSize: 12,
+            },
+          },
+          splitLine: {
+            lineStyle: {
+              color: "rgba(255,255,255,.1)",
+            },
+          },
+        },
+      ],
+      legend: {
+        right: "0%",
+        data: data.xAxis,
+        textStyle: {
+          color: "rgba(255,255,255,.5)",
+          fontSize: "12",
+        },
+      },
+      series: [
+        {
+          name: "时段在馆人数/总座位数",
+          type: "line",
+          smooth: true,
+          symbol: "circle",
+          symbolSize: 5,
+          showSymbol: false,
+          data: data.data,
           lineStyle: {
-            color: "rgba(255,255,255,.1)",
+            normal: {
+              color: "#0184d5",
+              width: 2,
+            },
+          },
+          areaStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(
+                0,
+                0,
+                0,
+                1,
+                [
+                  {
+                    offset: 0,
+                    color: "rgba(1, 132, 213, 0.4)",
+                  },
+                  {
+                    offset: 0.8,
+                    color: "rgba(1, 132, 213, 0.1)",
+                  },
+                ],
+                false
+              ),
+              shadowColor: "rgba(0, 0, 0, 0.1)",
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: "#0184d5",
+              borderColor: "rgba(221, 220, 107, .1)",
+              borderWidth: 12,
+            },
+          },
+          label: {
+            show: true, // 显示标签
+            position: "top", // 标签位置，默认值是 'top'
+            formatter: "{c}%", // 标签格式化函数，这里表示显示数据值并加上百分号
+            textStyle: {
+              color: "rgba(255, 255, 255, .7)", // 标签文字颜色
+              fontSize: 12, // 标签文字字号
+            },
           },
         },
-        axisLabel: {
-          formatter: "{value}%", // 设置刻度标签的格式为百分比形式
-          textStyle: {
-            color: "rgba(255,255,255,.6)",
-            fontSize: 12,
-          },
-        },
-        splitLine: {
-          lineStyle: {
-            color: "rgba(255,255,255,.1)",
-          },
-        },
-      },
-    ],
-    legend: {
-      right: "0%",
-      textStyle: {
-        color: "rgba(255,255,255,.5)",
-        fontSize: "12",
-      },
-    },
-    series: [
-      {
-        name: "时段在馆人数/总座位数",
-        type: "line",
-        smooth: true,
-        symbol: "circle",
-        symbolSize: 5,
-        showSymbol: false,
-        lineStyle: {
-          normal: {
-            color: "#0184d5",
-            width: 2,
-          },
-        },
-        areaStyle: {
-          normal: {
-            color: new echarts.graphic.LinearGradient(
-              0,
-              0,
-              0,
-              1,
-              [
-                {
-                  offset: 0,
-                  color: "rgba(1, 132, 213, 0.4)",
-                },
-                {
-                  offset: 0.8,
-                  color: "rgba(1, 132, 213, 0.1)",
-                },
-              ],
-              false
-            ),
-            shadowColor: "rgba(0, 0, 0, 0.1)",
-          },
-        },
-        itemStyle: {
-          normal: {
-            color: "#0184d5",
-            borderColor: "rgba(221, 220, 107, .1)",
-            borderWidth: 12,
-          },
-        },
-        label: {
-          show: true, // 显示标签
-          position: "top", // 标签位置，默认值是 'top'
-          formatter: "{c}%", // 标签格式化函数，这里表示显示数据值并加上百分号
-          textStyle: {
-            color: "rgba(255, 255, 255, .7)", // 标签文字颜色
-            fontSize: 12, // 标签文字字号
-          },
-        },
-      },
-    ],
-  };
-  // 使用刚指定的配置项和数据显示图表。
-  echarts_4.setOption(option);
+      ],
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    echarts_4.setOption(option);
+  });
   window.addEventListener("resize", function () {
     echarts_4.resize();
   });
 }
-function updateChart_4() {
+function updateChart4() {
   // 发起异步请求获取数据
   $.get("/api/hourly_utilization").done(function (data) {
     // 根据获取的数据更新图表的配置项和数据
